d9f817cfc631a36d76c1bf4fd9ac162836f3dd6b
Daniel Garon <dgaron@colostate.edu>
2023-08-12 14:57:24 -0600

    Corrects bug that ignored final block

--- core/ledger/kvledger/history/query_executer.go
+++ core/ledger/kvledger/history/query_executer.go
@@ -77,12 +77,16 @@ type historyScanner struct {
 }
 
 func (scanner *historyScanner) Next() (commonledger.QueryResult, error) {
-	if scanner.txIndex <= -1 {
-		if scanner.currentBlock == scanner.previousBlock {
+	if scanner.txIndex == -1 {
+		oldBlockNum := scanner.currentBlock
+		scanner.updateBlock()
+		if scanner.currentBlock == oldBlockNum {
+			// Iterator exhausted
 			return nil, nil
 		}
-		scanner.updateBlock()
 	}
+
+
 
 	blockNum := scanner.currentBlock
 	tranNum := scanner.transactions[scanner.txIndex]
